a5cafb36a299078703d2cb71e532da4f2c1e11c8
spekary <spekary@gmail.com>
2022-01-25 14:36:11 -0800

    Adding path command

---
+++ internal/cmd/outPath.go
@@ -0,0 +1,17 @@
+// Copyright 2018 Shannon Pekary. All rights reserved.
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func outPath(cmd *cobra.Command, args []string) error {
+	f := processFileArg(args[0])
+	_,_ = fmt.Fprintf(cmd.OutOrStdout(), f)
+	return nil
+}
+
